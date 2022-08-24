c9997014ed240ae8bf3c8bd029b6c1aa7a45b516
schunlei <48410012+schunlei@users.noreply.github.com>
2022-08-24 23:32:59 +0800

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1780,7 +1780,7 @@ define Device/wavlink_wl-wn531ax2
   $(Device/uimage-lzma-loader)
   DEVICE_VENDOR := Wavlink
   DEVICE_MODEL := WL-WN531AX2
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt7915e kmod-usb3
+  DEVICE_PACKAGES := kmod-mt7915e kmod-usb3
   KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb | pad-to 64k
   KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd
   IMAGE_SIZE := 15040k
