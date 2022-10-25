c8bca46ff8ff05ff0295d353773b63950e9fed8c
schunlei <48410012+schunlei@users.noreply.github.com>
2022-10-25 11:08:08 +0800

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1784,7 +1784,7 @@ define Device/wavlink_wl-wn531ax2
   DEVICE_PACKAGES := kmod-mt7915e kmod-usb3
 #  KERNEL_LOADADDR := 0x81001000
   KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd
+#  KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd
   IMAGE_SIZE := 15040k
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | pad-rootfs | \
 	check-size | append-metadata
